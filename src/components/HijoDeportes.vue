eafa2ccc8e31b3e312b872c77d7ce8503469d817
luismi937 <luismicanizares02@gmail.com>
2025-10-27 14:14:47 +0100

    up

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hijo deportes</h1>
+        <h4 style="color:blue">{{nombredeporte}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HijoDeportes",
+    props: ["nombredeporte"]
+    , mounted(){
+        console.log(this.nombredeporte)
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Padre Deportes</h1>
+        <ul>
+            <li v-for="sport in deportes" :key="sport">
+                <HijoDeportes :nombredeporte="sport"/>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+ import HijoDeportes from './HijoDeportes.vue'
+export default {
+    name: "PadreDeportes",
+    components:{
+        HijoDeportes
+    },
+    data(){
+        return{
+            deportes:["Padel", "tenis", "futbol", "waterpolo"]
+        }
+    }
+   
+
+}
+</script>
+<style>
+
+</style>
